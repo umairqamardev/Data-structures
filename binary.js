604fbe8791ce41524cbb8906917b9c4c0c28294a
Umair Qamar <144217375+umairqamardev@users.noreply.github.com>
2026-02-14 17:16:57 +0500

    Month 2 - Sorting complete

---
+++ binary.js
@@ -0,0 +1,17 @@
+let arr = [1, 3, 5, 7, 9, 11, 13];
+let target = 11;
+let left = 0;
+let right = arr.length - 1;
+
+while(left <= right){
+    let mid = Math.floor((left + right) / 2);
+    
+    if(arr[mid] == target){
+        console.log("Found at index:", mid);
+        break;
+    } else if(target > arr[mid]){
+        left = mid + 1;
+    } else {
+        right = mid - 1;
+    }
+}
